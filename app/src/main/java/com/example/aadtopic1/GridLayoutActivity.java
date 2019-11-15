b8fa79aaccc676a59d978a6978191e9799a42fa2
Prerana07 <preranapandit07@gmail.com>
2019-11-15 15:23:12 +0545

    Complete layout design of calculator with 5 rows and 4 columns in gridlayout activity

---
+++ app/src/main/java/com/example/aadtopic1/GridLayoutActivity.java
@@ -0,0 +1,15 @@
+package com.example.aadtopic1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GridLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_layout);
+
+    }
+}
